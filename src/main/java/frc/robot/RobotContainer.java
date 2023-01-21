4d086d14cabbd485fdc8f71c43538b05f8bfbf14
Andrew <andrew.kepych@gmail.com>
2023-01-21 15:58:28 -0600

    initial commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,90 @@
+package frc.robot;
+
+import java.io.IOException;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import frc.robot.autos.*;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+    /* Controllers */
+    private final Joystick controller = new Joystick(0);
+
+    /* Drive Controls */
+    private final int translationAxis = XboxController.Axis.kLeftY.value;
+    private final int strafeAxis = XboxController.Axis.kLeftX.value;
+    private final int rotationAxis = XboxController.Axis.kRightX.value;
+
+    /* Driver Buttons */
+    private final JoystickButton zeroGyro = new JoystickButton(controller, XboxController.Button.kStart.value);
+    private final JoystickButton robotCentric = new JoystickButton(controller, XboxController.Button.kLeftBumper.value);
+
+    /* Subsystems */
+    private final Swerve m_SwerveSubsystem = new Swerve();
+
+    /* Trajectories */
+    Trajectory testPath = new Trajectory();
+
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer() {
+        m_SwerveSubsystem.setDefaultCommand(
+            new TeleopSwerve(
+                m_SwerveSubsystem, 
+                () -> controller.getRawAxis(translationAxis)*.75, 
+                () -> controller.getRawAxis(strafeAxis) * .75, 
+                () -> controller.getRawAxis(rotationAxis) *.75, 
+                () -> robotCentric.getAsBoolean()
+            )
+        );
+
+        try {
+            testPath = TrajectoryUtil.fromPathweaverJson(
+                Filesystem.getDeployDirectory().toPath().resolve(
+                    "pathplanner/generatedJSON/TEST.wpilib.json"));
+
+         } catch (IOException ex) {
+            DriverStation.reportError("Unable to open trajectory", ex.getStackTrace());
+         }
+
+        // Configure the button bindings
+        configureButtonBindings();
+    }
+
+    /**
+     * Use this method to define your button->command mappings. Buttons can be created by
+     * instantiating a {@link GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
+        /* Driver Buttons */
+        zeroGyro.onTrue(new InstantCommand(() -> m_SwerveSubsystem.zeroGyro()));
+    }
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // An ExampleCommand will run in autonomous
+        return new exampleAuto(m_SwerveSubsystem, testPath);
+    }
+}
---
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -0,0 +1,46 @@
+package frc.robot.commands;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.Swerve;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+
+public class TeleopSwerve extends CommandBase {    
+    private Swerve s_Swerve;    
+    private DoubleSupplier translation;
+    private DoubleSupplier strafe;
+    private DoubleSupplier rotation;
+    private BooleanSupplier robotCentric;
+
+    public TeleopSwerve(Swerve m_SwerveSubsystem, DoubleSupplier translationSup, DoubleSupplier strafeSup, DoubleSupplier rotationSup, BooleanSupplier robotCentricSup) {
+        this.s_Swerve = m_SwerveSubsystem;
+        addRequirements(m_SwerveSubsystem);
+
+        this.translation = translationSup;
+        this.strafe = strafeSup;
+        this.rotation = rotationSup;
+        this.robotCentric = robotCentricSup;
+    }
+
+    @Override
+    public void execute() {
+        /* Get Values, Deadband*/
+        double translationVal = MathUtil.applyDeadband(translation.getAsDouble(), Constants.stickDeadband);
+        double strafeVal = MathUtil.applyDeadband(strafe.getAsDouble(), Constants.stickDeadband);
+        double rotationVal = MathUtil.applyDeadband(rotation.getAsDouble(), Constants.stickDeadband);
+
+        /* Drive */
+        s_Swerve.drive(
+            new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed), 
+            rotationVal * Constants.Swerve.maxAngularVelocity, 
+            !robotCentric.getAsBoolean(), 
+            true
+        );
+    }
+}
